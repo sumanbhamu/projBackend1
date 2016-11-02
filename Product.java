573b11415770feb4f3adce395116826022d26b64
suman <sumanbhamu@rocketmail.com>
2016-11-02 06:53:40 -0700

    Add files via upload
    creating private variables and setter/getter methods

---
+++ Product.java
@@ -0,0 +1,52 @@
+package com.niit.shopping.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+import org.springframework.web.multipart.MultipartFile;
+
+@Entity
+@Table(name="product")
+public class Product 
+{
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int prod_id;
+	private String prod_name;
+	private int prod_price;
+	private int quantity;
+	
+	@Transient
+	private MultipartFile img;
+	public int getProd_id() {
+		return prod_id;
+	}
+	public void setProd_id(int prod_id) {
+		this.prod_id = prod_id;
+	}
+	public String getProd_name() {
+		return prod_name;
+	}
+	public void setProd_name(String prod_name) {
+		this.prod_name = prod_name;
+	}
+	public int getProd_price() {
+		return prod_price;
+	}
+	public void setProd_price(int prod_price) {
+		this.prod_price = prod_price;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	
+	
+	
+}
--- User.java
+++ User.java
@@ -1,13 +1,24 @@
-//its a pojo or do(domain object)
+//its a pojo or do(domain object)--model
 package com.niit.shopping.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/* Entity refers to the class to be converted into table */
+@Entity
+/* Table specifies the table name */
+@Table(name="user")
 public class User
 {
+	/* ID refers to the Primary key*/
+	@Id
 	private int user_id;
 	private String username;
 	private String password;
 	private String emailid;
 	private String phno;
+	//enabled refers if user is enabled or not
 	private String enabled;
 	public int getUser_id() {
 		return user_id;
@@ -16,6 +27,8 @@ public class User
 		this.user_id = user_id;
 	}
 	public String getUsername() {
+		System.out.println("User Class in backend Model");
+		
 		return username;
 	}
 	public void setUsername(String username) {
