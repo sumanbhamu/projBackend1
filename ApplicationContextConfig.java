9a285e06628e61d4ba80f09b1b39f3b647b26b52
suman <sumanbhamu@rocketmail.com>
2016-11-03 09:44:06 -0700

    Add files via upload

--- ApplicationContextConfig.java
+++ ApplicationContextConfig.java
@@ -18,10 +18,13 @@ import com.niit.shopping.dao.CategoryDAO;
 import com.niit.shopping.dao.CategoryDAOImpl;
 import com.niit.shopping.dao.ProductDAO;
 import com.niit.shopping.dao.ProductDAOImpl;
+import com.niit.shopping.dao.SupplierDAO;
+import com.niit.shopping.dao.SupplierDAOImpl;
 import com.niit.shopping.dao.UserDAO;
 import com.niit.shopping.dao.UserDAOImpl;
 import com.niit.shopping.model.Category;
 import com.niit.shopping.model.Product;
+import com.niit.shopping.model.Supplier;
 import com.niit.shopping.model.User;
 
 @Configuration
@@ -45,6 +48,7 @@ public class ApplicationContextConfig
 	    properties.put("hibernate.show_sql", "true");
 	    properties.put("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
 	    properties.put("hibernate.hbm2ddl.auto", "update");
+	    //treat every session as a thread
 	    properties.put("hibernate.current_session_context_class", "thread");
 	    System.out.println("hbernate properties");
 	    return properties;
@@ -56,18 +60,21 @@ public class ApplicationContextConfig
 	    LocalSessionFactoryBuilder sessionBuilder = new LocalSessionFactoryBuilder(dataSource);
 	    sessionBuilder.addProperties(getHibernateProperties());
 	 
-	/* sessionBuilder.addAnnotatedClasses(Category.class);
-	  sessionBuilder.addAnnotatedClasses(Supplier.class);
-	   sessionBuilder.addAnnotatedClasses(UserDetails.class);
+	/* sessionBuilder.addAnnotatedClasses(UserDetails.class);
 	   sessionBuilder.addAnnotatedClasses(Cart.class);
 	   sessionBuilder.addAnnotatedClasses(CartItem.class);
 	   sessionBuilder.addAnnotatedClasses(Customer.class);
 	   
 	*/   
-	    sessionBuilder.addAnnotatedClasses(Product.class);
-		  
+	     
 	    sessionBuilder.addAnnotatedClasses(User.class);
+	    sessionBuilder.addAnnotatedClasses(Product.class);
+	    sessionBuilder.addAnnotatedClasses(Category.class);
+	    sessionBuilder.addAnnotatedClasses(Supplier.class);
+		     
+		
 	    System.out.println("session factory");
+	    
 	    return sessionBuilder.buildSessionFactory();
 	}
 	
@@ -80,20 +87,7 @@ public class ApplicationContextConfig
 	    System.out.println("transaction ");
 	    return transactionManager;
 	}
-	   /*@Autowired
-		@Bean(name="categoryDAO")
-		public CategoryDAO getCategoryDAO(SessionFactory sessionFactory) {
-		    
-		 
-		    return new CategoryDAOImpl( sessionFactory);
-		}
-	    @Autowired
-	    @Bean(name="category")
-	    public Category getCategory()
-	    {
-	    	return new Category();
-	    }
-*/
+	  
 /*
 	    @Autowired
 		@Bean(name="supplierDAO")
@@ -110,6 +104,8 @@ public class ApplicationContextConfig
 	    	return new Supplier();
 	    }
 */	   @Autowired
+
+//product
 		@Bean(name="userDAO")
 		public UserDAO getUserDetailsDAO(SessionFactory sessionFactory) {
 		    
@@ -123,23 +119,64 @@ public class ApplicationContextConfig
 	    	System.out.println("user");
 	    	return new User();
 	    }
-
-	    
+//product
 	    
 	    @Autowired
 		@Bean(name="productDAO")
 		public ProductDAO getProductDAO(SessionFactory sessionFactory) {
 		    
-		 
+	    	System.out.println("product dao");
+	    	
 		    return new ProductDAOImpl(sessionFactory);
 		}
 	    @Autowired
 	    @Bean(name="product")
 	    public Product getProduct()
 	    {
+	    	System.out.println("product");
+	    	
 	    	return new Product();
 	    }
 	   
+	    
+//category	    
+	    
+	    @Autowired
+		@Bean(name="categoryDAO")
+		public CategoryDAO getCategoryDAO(SessionFactory sessionFactory) {
+		    
+	    	System.out.println("category dao");
+	    	
+		    return new CategoryDAOImpl( sessionFactory);
+		}
+	    @Autowired
+	    @Bean(name="category")
+	    public Category getCategory()
+	    {
+	    	System.out.println("category ");
+	    	
+	    	return new Category();
+	    }
+
+//supplier	    
+	    
+	    @Autowired
+		@Bean(name="supplierDAO")
+		public SupplierDAO getSupplierDAO(SessionFactory sessionFactory) {
+		    
+	    	System.out.println("supplier dao");
+	    	
+		    return new SupplierDAOImpl( sessionFactory);
+		}
+	    @Autowired
+	    @Bean(name="supplier")
+	    public Supplier getSupplier()
+	    {
+	    	System.out.println("supplier ");
+	    	
+	    	return new Supplier();
+	    }
+
 
 
 
